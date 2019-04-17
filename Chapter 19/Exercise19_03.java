f8a898da9cfa8e5281b769e20d44688a21caed7b
adamanderson166 <43416459+adamanderson166@users.noreply.github.com>
2019-04-17 14:36:49 -0600

    Add files via upload

---
+++ Chapter 19/Exercise19_03.java
@@ -0,0 +1,35 @@
+/*
+Author: Adam Anderson
+Date: 4/8/2019
+
+Description: Write the following method that returns a new ArrayList. The new list contains the non-duplicate elements from the original list. Use Exercise19_03.javaPreview the document as the test class.
+ */
+import java.util.ArrayList;
+
+public class Exercise19_03 {
+  public static void main(String[] args) {
+    
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(14);
+    list.add(42);
+    list.add(25);
+
+    ArrayList<Integer> fixed = removeDuplicates(list);
+    
+    System.out.println("The old list contains:" + list);
+    System.out.println("The adjusted list contains:" + fixed);
+  }
+  
+  // This remove duplicates function removes any duplicates
+  
+  public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+    ArrayList<E> fixed = new ArrayList<>();
+    for (int i = 0; i < list.size(); i++)
+      if (!fixed.contains(list.get(i))) 
+        fixed.add(list.get(i));
+        return fixed;
+
+  }
+}
---
+++ Chapter 19/Exercise19_09.java
@@ -0,0 +1,46 @@
+/*
+Author: Adam Anderson
+Date: 4/9/2019
+
+Description: (Sort ArrayList) Write the following method that sorts an ArrayList. Use Exercise19_03.java as the test class.
+*/
+import java.util.ArrayList;
+
+public class Exercise19_09 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(4);
+    list.add(42);
+    list.add(5);
+    
+    System.out.println("Here's what the first Array List looks like: " + list);
+    Exercise19_09.<Integer>sort(list);
+    System.out.println("");
+    System.out.println("Here's what the sorted Array List looks like: " + list);
+  }
+  
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+        
+        //declaring the currentMin variable
+        E currentMin;
+        //declaring min variable
+        int min;
+        for (int i = 0; i < list.size(); i++) {
+          currentMin = list.get(i);
+          min = i;
+          for(int j = i + 1; j < list.size(); j++) {
+            if(currentMin.compareTo(list.get(j)) > 0) {
+              min = j;
+              currentMin = list.get(j);
+            }
+        }
+        
+        if (min != i) {
+          list.set(min, list.get(i));
+          list.set(i, currentMin);
+        }
+      }
+    }
+}
